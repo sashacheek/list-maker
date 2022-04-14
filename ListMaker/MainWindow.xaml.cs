0f3ff084177c698094fd38cf5827e8750a73d93b
sashacheek <sasha@cheek.tv>
2022-04-14 15:38:48 -0400

    + Button adds new textBox row

--- ListMaker/MainWindow.xaml.cs
+++ ListMaker/MainWindow.xaml.cs
@@ -24,5 +24,63 @@ namespace ListMaker
         {
             InitializeComponent();
         }
+
+        private void Add_Row(object sender, RoutedEventArgs e)
+        {
+            //RowDefinition rowDefinition = new RowDefinition();
+            //rowDefinition.Height = GridLength.Auto;
+            //CheckList.Add(rowDefinition);
+
+            //// TextBox selectedTextBox = (TextBox)CheckList.SelectedItem;
+
+            TextBox textBox = new TextBox();
+            textBox.AcceptsReturn = true;
+            textBox.TextWrapping = TextWrapping.Wrap;
+            textBox.Padding = new Thickness(5);
+            CheckList.Items.Add(textBox);
+
+            //Grid.SetRow(textBox, CheckList.RowDefinitions.Count - 1);
+
+            //foreach (ListViewItem listViewItem in CheckList.Items)
+            //{
+
+            //}
+
+
+            //CheckList.Items.Add(textBox);
+
+            //string item = CheckList.SelectedIndex.ToString();
+            //MessageBox.Show(item);
+
+            //ListView listView = new ListView();
+            //int i = 0;
+
+            // listView.Items.Add(item);
+            //foreach (ListItem listViewItem in CheckList.Items)
+            //{
+
+            //    if (i == CheckList.SelectedIndex)
+            //    {
+            //        TextBox textBox = new TextBox();
+            //        textBox.AcceptsReturn = true;
+            //        textBox.TextWrapping = TextWrapping.Wrap;
+            //        textBox.Padding = new Thickness(5);
+            //        listView.Items.Add(textBox);
+            //    }
+            //    TextBox textBox2 = new TextBox();
+            //    textBox2.AcceptsReturn = true;
+            //    textBox2.TextWrapping = TextWrapping.Wrap;
+            //    textBox2.Padding = new Thickness(5);
+            //    textBox2.Text = listViewItem.Text;
+            //    listView.Items.Add(textBox2);
+
+            //listView.Items.Add(listViewItem);
+            //    i++;
+            //}
+
+            //CheckList = listView;
+
+        }
+
     }
 }
